3d51e6d900189e599024e5eadbc918d22683621e
Purhita <purhito@gmail.com>
2025-05-15 11:35:12 +0900

    simpan ke csv

--- web_scrapping_03.py
+++ web_scrapping_03.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import pandas
 
 file_web = open('contoh_web.html', mode='r')
 hal_web = file_web.read()
@@ -20,5 +21,15 @@ isi_kolom = []
 for item in tag_isi_kolom:
     isi_kolom.append(item.getText())
 
-print(judul_kolom)
-print(isi_kolom)
+bahasa_pemrograman = (isi_kolom[0::3])
+waktu_kursus = (isi_kolom[1::3])
+biaya_kursus = (isi_kolom[2::3])
+
+informasi_kursus = {
+    judul_kolom[0]: bahasa_pemrograman,
+    judul_kolom[1]: waktu_kursus,
+    judul_kolom[2]: biaya_kursus
+}
+
+df_informasi_kursus = pandas.DataFrame(informasi_kursus)
+df_informasi_kursus.to_csv ('hasil_scrapping_03.csv')
